ea3208aaebc103cf60708b6b7cddd3ca08be868b
Felix MIL <felix.mil@esqlabs.com>
2024-01-11 16:49:25 +0100

    Rename and add documentation + add TODOs + add test

--- R/Calculate_PartitionsC.R
+++ R/Calculate_PartitionsC.R
@@ -1,84 +1,96 @@
-calcPartitions<-function(LogP,hlcAt,MW,pKa,typeIonization,assumptions,fu,BC){
-
-  #Calculate ionization
-  if(typeIonization=="acid"){
-  fNeutral=1/(1+10**(pH-pKa))
-  }else if (typeIonization=="base"){
-  fNeutral=1/(1+10**(pKa-pH))
-  }else{
-  fNeutral=1
+#' getInVitroFractionUnbound
+#'
+#' @description
+#' Compute the fraction unbound in vitro
+#'
+#' @param LogP LogP of the compound
+#' @param hlcAt Henry's Law Constant in atm/(m3*mol)
+#' @param MW Molecular Weight of the compound
+#' @param pKa pkA of the compound
+#' @param ionization type of ionization (acid, base, none)
+#' @param assumptions type of assumption used (1: Poulin and Theil, 2: PK-Sim® Standard, 3: Rodgers & Rowland, 4: Schmidtt)
+#' @param fu fraction unbound
+#' @param BC
+#'
+#' @return
+#' @export
+#'
+#' @details
+#' \deqn{fu_{in vitro}=1/(1 + C_{cells} \times 10^{0.4 \times logP - 1.38} )}
+getInVitroFractionUnbound <- function(LogP, hlcAt, MW, pKa, ionization, assumptions, fu, BC) {
+  # Calculate ionization
+  if (ionization == "acid") {
+    fNeutral <- 1 / (1 + 10**(pH - pKa))
+  } else if (ionization == "base") {
+    fNeutral <- 1 / (1 + 10**(pKa - pH))
+  } else {
+    fNeutral <- 1
   }
 
-  #Partitions to different components
-  kOW=10**logP
+  # Partitions to different components
+  kOW <- 10**logP
 
-  #Calculate air-water partition coefficient
-  #Divide henry law constant in atm/(m3*mol) with the temperature in kelvin and gas constant R (j/k*mol)
-  kAir=hlcAt/(0.08206*310)
+  # Calculate air-water partition coefficient
+  # Divide henry law constant in atm/(m3*mol) with the temperature in kelvin and gas constant R (j/k*mol)
+  kAir <- hlcAt / (0.08206 * 310)
 
-  #Calculate plastic partitioning
-  kPlasticFischer=10**(logP*0.47-4.64)
-  kPlasticKramer=10**(logP*0.97-6.94)
-  kPlastic=mean(kPlasticFischer,kPlasticKramer)*fNeutral
+  # Calculate plastic partitioning
+  kPlasticFischer <- 10**(logP * 0.47 - 4.64)
+  kPlasticKramer <- 10**(logP * 0.97 - 6.94)
+  kPlastic <- mean(kPlasticFischer, kPlasticKramer) * fNeutral
 
-  #QSPRs for calculating partitioning in in vitro
-  if ("assumption"=="Poulin and Theil"){
-    #Calculate protein partitioning
+  # QSPRs for calculating partitioning in in vitro
+  if ("assumption" == "Poulin and Theil") {
+    # Calculate protein partitioning
 
-    #Calculate lipid partitioning
-    kNLip= fNeutral*kOW
+    # Calculate lipid partitioning
+    kNLip <- fNeutral * kOW
 
-  fuInvitro=1/(1+kNLip*cLip+kPlastic*saPlasticVolMedium)
+    fuInvitro <- 1 / (1 + kNLip * cLip + kPlastic * saPlasticVolMedium)
+  } else if ("assumption" == "PK-Sim® Standard") {
 
-  }else if ("assumption"=="PK-Sim® Standard"){
+  } else if ("assumption" == "Rodgers & Rowland") {
+    Hema <- 0.45
+    kpuBC <- (Hema - 1 + BP) / (Hema * fu)
+    kAPLip_1 <- kpuBC -
+      ((1 + 10**(pKa - pHBC)) / (1 + 10**(pKa - pH)) * fiwBC) -
+      (kOW * fnlBC + (0.3 * kOW + 0.7) * fnpBC) / (1 + 10**(pKa - pH))
 
-  }else if ("assumption"=="Rodgers & Rowland"){
+    kAPBC <- kAPLip_1 * (1 + 10**(pKa - pH)) / (0.5 * 10**(pKa - pHBC))
+    kAPLip <- (kAPBC * 10**(pKa - pH)) * fNeutral
+    kNLip <- fNeutral * kOW
+    if (ionization == "base") {
+      fuInvitro <- 1 / (1 + kNLip * cNLip +
+        (kNLip * 0.3 + 0.7) * cNPLip +
+        kAPLip * cAPLip + # need to check units of APL ( rodgers are in mg/g)
+        kPla * saPlasticVolMedium)
+    } else {
+      fuInvitro <- 1 / (1 + kNLip * cNLip +
+        (kNLip * 0.3 + 0.7) * cNPLip +
+        kPla * saPlasticVolMedium)
+    }
+  } else if ("assumption" == "Schmitt") {
+    K1 <- F1 * F2 * F3
+    F1 * F2 * F3 + K2 * base^CT0 + K3 * base^CT1 + K4 * base^CT2 + K5 * base^(CT0 + CT1) + K6 * base^(CT0 + CT2) + K7 * base^(CT2 + CT1) + K8 * base^(CT0 + CT1 + CT2)
 
-      Hema=0.45
-      kpuBC=(Hema-1+BP)/(Hema*fu)
-      kAPLip_1=kpuBC-
-        ((1+10**(pKa-pHBC))/(1+10**(pKa-pH))*fiwBC)-
-        (kOW*fnlBC+(0.3*kOW+0.7)*fnpBC)/(1+10**(pKa-pH))
+    Kprot <- (0.81 + 0.11 * 10^LogMA) / 24.92 * 5.0
+    KnPL <- 10**LogMA
 
-      kAPBC=kAPLip_1*(1+10**(pKa-pH))/(0.5*10**(pKa-pHBC))
-      kAPLip= (kAPBC*10**(pKa-pH))*fNeutral
-      kNLip= fNeutral*kOW
-      if(typeIonization=="base"){
-          fuInvitro=1/(1+kNLip*cNLip+
-                         (kNLip*0.3+0.7)*cNPLip+
-                         kAPLip*cAPLip+ #need to check units of APL ( rodgers are in mg/g)
-                         kPla*saPlasticVolMedium)
-      }else{
-          fuInvitro=1/(1+kNLip*cNLip+
-                         (kNLip*0.3+0.7)*cNPLip+
-                         kPla*saPlasticVolMedium)
-      }
-
-  }else if ("assumption"=="Schmitt"){
-    K1=F1*F2*F3
-    F1*F2*F3+ K2* base ^ CT0 + K3* base ^ CT1+K4 *base^CT2 + K5* base ^ (CT0+CT1) +K6* base ^ (CT0+CT2) +K7* base ^ (CT2+CT1) +K8* base ^ (CT0+CT1+CT2)
-
-    Kprot=(0.81 + 0.11 * 10^LogMA) / 24.92 * 5.0
-    KnPL=10**LogMA
-
-    K_a_PL=K_n_PL*K_a_PL_pH_Factor
-    fuInVitro=1/(1+KnL * f_nl + KnPL * f_np + KaPL * AP_T + KProt * f_proteins)
-
-  }else if ("assumption"=="Berezhkovskiy"){
-
-    fuInvitro=1 / (10^LogP * (V_nlp + 0.3 * V_php) +  0.7 * V_php + V_wp / fu)
-
-  }else if ("assumption"=="Berezhkovskiy"){
+    K_a_PL <- K_n_PL * K_a_PL_pH_Factor
+    fuInVitro <- 1 / (1 + KnL * f_nl + KnPL * f_np + KaPL * AP_T + KProt * f_proteins)
+  } else if ("assumption" == "Berezhkovskiy") {
+    fuInvitro <- 1 / (10^LogP * (V_nlp + 0.3 * V_php) + 0.7 * V_php + V_wp / fu)
+  } else if ("assumption" == "Berezhkovskiy") {
 
 
-  }else {}
+  } else {}
 
 
-  #Warning for volatility
-  fuAir=fuInvitro*kAir*volAir_L
-  if (fuAir>0.1){
-    print("Probable evaporation")
-  }else {}
+  # Warning for volatility
+  fuAir <- fuInvitro * kAir * volAir_L
+  if (fuAir > 0.1) {
+    warning("Probable evaporation")
+  } else {}
 
   return(fuInvitro)
 }
---
+++ tests/testthat/test-in-vitro-compartment.R
@@ -0,0 +1,3 @@
+test_that("generating an in vitro compartment works", {
+  comp <- getInVitroCompartment(1, 1, 96, 0.392)
+})
