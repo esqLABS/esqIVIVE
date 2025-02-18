79fde5c95d3ed347987b10da2b1462889b6b67db
Felix MIL <felix.mil@esqlabs.com>
2025-02-18 16:00:38 +0100

    fix example

--- R/Calculate_Fu_inc_mic.R
+++ R/Calculate_Fu_inc_mic.R
@@ -19,9 +19,9 @@
 #' @export
 #'
 #' @examples
-FractionUnbound(partitionQSPR="All PK-Sim Standard",logLipo=3,ionization=c("acid",0,0),
-                           typeSystem="hepatocytes",FBS=0,microplateType=96,
-                           volMedium=0.22,pKa=c(6,0,0),hlcAt=1E-6,cCells=2)
+#'FractionUnbound(partitionQSPR="All PK-Sim Standard",logLipo=3,ionization=c("acid",0,0),
+#'                           typeSystem="hepatocytes",FBS=0,microplateType=96,
+#'                           volMedium=0.22,pKa=c(6,0,0),hlcAt=1E-6,cCells=2)
 #'
 #' @details
 #'
