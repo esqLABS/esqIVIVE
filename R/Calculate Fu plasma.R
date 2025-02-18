c0635f417edc2400e6616a75e72141b62d4788e0
Susana Proença <susana.proenca@esqlabs.com>
2025-02-18 17:37:27 +0000

    updating functions

--- R/Calculate Fu plasma.R
+++ R/Calculate Fu plasma.R
@@ -72,41 +72,66 @@ convertKintoFu <- function(khsa_Lkg, kglob_Lkg, kmemlip_LL, klip_LL, species) {
 #              "LFER_V"=1.39)
 
 
-QSARs_plasma <- function(QSAR, logP, pKa, ionization,
-                         LFER_E = NULL, LFER_B = NULL, LFER_A = NULL, LFER_S = NULL, LFER_V = NULL) {
-  # run function to get ionization factors
-  fneutral <- getIonization(ionization, pKa)
-  X <- fneutral["fneutral_plasma"] # Interstitial tissue
-  Y <- fneutral["fneutral_cells"] # intracellular
+QSARs_plasma<-function(QSAR,logP,pKa,ionization,LFER_E=NULL,LFER_B=NULL,LFER_A=NULL,LFER_S=NULL,LFER_V=NULL){
+  #run function to get ionization factors
 
-  if (QSAR == "logP") {
-    logD <- logP * 1 / (1 + X)
-    kmemlip_LL <- 10^logD
-    # for albumin we are not correcting for ionization since acid molecules also bind albumin
-    khsa_kgL <- 0.163 + 0.0221 * 10^logP # Schmitt equation, check where is it based and which equation was used in the VCBA
-    kglob_kgL <- 0.163 + 0.0221 * 10^logD
-  } else if (QSAR == "PPLFER") {
-    # Add LFER_a
-    LFER_Ei <- 0.15 + LFER_E
-    LFER_Vi <- -0.0215 + LFER_V
-    LFER_Bi <- 2.15 - 0.204 * LFER_S + 1.217 * LFER_B + 0.314 * LFER_V
-    LFER_Si <- 1.224 + 0.908 * LFER_E + 0.827 * LFER_S + 0.453 * LFER_V
-    LFER_Ai <- -0.208 - 0.058 * LFER_S + 0.0354 * LFER_A + 0.076 * LFER_V
-    LFER_J <- 1.793 + 0.267 * LFER_E - 0.195 * LFER_S + 0.35 * LFER_V
+  source("functions/Ionization.R")
 
-    # check units
-    # check possibly appli limit, range chemicals...
-    kmemlip_LL <- 10^(0.29 + 0.74 * LFER_E - 0.72 * LFER_S - 3.63 * LFER_B + 3.3 * LFER_V)
-    kbsa_kgL <- 0.29 + 0.36 * LFER_E - 0.26 * LFER_S - 3.23 * LFER_B + 2.82 * LFER_V
-    # equation for ions from https://pubs.acs.org/doi/10.1021/acs.est.5b06176
-    kbsa_kgL <- 0.85 + 0.63 * LFER_Ei - 0.63 * LFER_Si - 0.05 * LFER_Ai + 2.08 * LFER_Bi + 2.06 * LFER_Vi + 3.13 * LFER_J
-    khsa_kgL <- kbsa_kgL
-    kmus_kg <- -0.24 + 0.68 * LFER_E - 0.76 * LFER_S - 2.29 * LFER_B + 2.51 * LFER_V
-    kglob_kgL <- kmus_kg
+  fneutral=getIonization(ionization,pKa)
+
+  X= fneutral["fneutral_plasma"] #Interstitial tissue
+
+  Y= fneutral["fneutral_cells"] #intracellular
+
+  if (QSAR=="logP"){
+
+    logD<-logP*1/(1+X)
+
+    if(pKa[1]!=0){
+      kmemlip_LL<-10^logD
+      } else {
+      #Yu et al  regression
+      kmemlip_LL<-10^(1.294+0.304*LogP)}
+
+    #for albumin we are not correcting for ionization since acid molecules also bind albumin
+
+    khsa_Lkg<-0.163+0.0221*kmemlip_LL #Schmitt equation
+
+    kglob_Lkg_1<-0.163+0.0221*kmemlip_LL #Schmitt equation for general tissue protein
+
+    kglob_Lkg_2<-10^(0.37*logD-0.29) #based on the eq used in the VCBA
+
+    kglob_Lkg<-mean(kglob_kgL_1,kglob_kgL_2)
+
+  }else if (QSAR=="PPLFER"){
+
+    #Add LFER_a
+
+    LFER_Ei=0.15+LFER_E
+
+    LFER_Vi=-0.0215+LFER_V
+
+    LFER_Bi=2.15-0.204*LFER_S+1.217*LFER_B+0.314*LFER_V
+
+    LFER_Si= 1.224+0.908*LFER_E+0.827*LFER_S+0.453*LFER_V
+
+    LFER_Ai=-0.208-0.058*LFER_S+0.0354*LFER_A+0.076*LFER_V
+
+    LFER_J=1.793+0.267*LFER_E-0.195*LFER_S+0.35*LFER_V
+
+
+    #check possibly appli limit, range chemicals...
+  kmemlip_LL=10^(0.29+0.74*LFER_E-0.72*LFER_S-3.63*LFER_B+3.3*LFER_V)
+  kbsa_kgL=0.29+0.36*LFER_E-0.26*LFER_S-3.23*LFER_B+2.82*LFER_V
+  #equation for ions from https://pubs.acs.org/doi/10.1021/acs.est.5b06176
+  kbsa_kgL=0.85+0.63*LFER_Ei-0.63*LFER_Si-0.05*LFER_Ai+2.08*LFER_Bi+2.06*LFER_Vi+3.13*LFER_J
+  khsa_kgL=kbsa_kgL
+  kmus_kg=-0.24+0.68*LFER_E-0.76*LFER_S-2.29*LFER_B+2.51*LFER_V
+  kglob_kgL=kmus_kg
+
   }
+return(c("kmemlip_LL"=kmemlip_LL,"khsa_Lkg"= khsa_Lkg,
+       "kglob_Lkg"=kglob_Lkg))
 
-  return(c(
-    "kmemlip_LL" = kmemlip_LL, "khsa_kgL" = khsa_kgL,
-    "kglob_kgL" = kglob_kgL
-  ))
+
 }
--- R/clearance_IVIVE.R
+++ R/clearance_IVIVE.R
@@ -1,246 +1,204 @@
-#' Clearance IVIVE
 #'
 #' @description
 #' IVIVE for clearance
 #'
+#'
 #' @param typeValue describe what type of in vitro data for clearance it is
-#' @param units this are the units of the value
+#' @param units this are the units of the value, does not matter typeValue is "decay experimentalcurve"
 #' @param typeSystem microsomes or hepatocytes
-#' @param expData depending on the type of value this can be a csv file or just one value, units are min or min-1
-#' @param partitionQSPR type of assumption used (Poulin and Theil, PK-Sim® Standard, Rodgers & Rowland, Schmidtt)
-#' @param logLipo LogP or LogMA of the compound
-#' @param FBS fraction of FBS or FCS used
-#' @param microplateType type of microplate, 96, 24, 48-wells, etc.
-#' @param ionization Vector of length 3 with ionization class, acid, neutral and base, if not input then it is c(0,0,0)
+#' @param expData depending on the type of value this can be a csv file or just one value
 #' @param volMedium volume of medium in the well (in mL)
 #' @param REF relative expression or activity factor
-#' @param hlcAt Henry's Law Constant in atm/(m3*mol)
-#' @param pKa pkA of the compound as a vector of length 3
-#' @param fu Fraction unbound in plasma
-#' @param fu_hep Fraction unbound in the in vitro hepatic system. if not known code will calculate it
+#' @param fu_invitro Fraction unbound in the in vitro hepatic system. if not known code will calculate it
 #' @param cMicro concentration of microsome protein mg/mL
 #' @param cCells concentration of hepatocytes used million cells/mL
-#' @param BP Blood plasma ratio
 #' @param species values can human and rat for now, defaulting to human
+#' @param tissue tissue of interest, since scaling factors are dependent on the tissue, will default to liver
 #'
-#' @return Specific clearance parameter to plug in PK-Sim
+#'@return  Specific clearance parameter to plug in PK-Sim
+#'@examples
 #'
-#' @export
+#'expData<-read.csv("tests/Clearance_example.csv",header=TRUE)
+#'clearance_IVIVE(typeValue="decay experimentalcurve",typeSystem="hepatocytes",cCells=0.5,units="/minutes",
+#'                expData=expData,fu_invitro=0.5)
 #'
-#' @examples
+#'example hepatocytes
+#' clearance_IVIVE(typeValue="in vitro clearance parameter",typeSystem="hepatocytes",units="mL/minutes/Millioncells",
+#' expData=18.27,fu_invitro=0.5,cCells=0.5)
 #'
-#' expData <- read.csv("tests/Clearance_example.csv", header = TRUE)
-#' library(ggplot2)
-#' clearance_IVIVE(
-#'   typeValue = "decay experimentalcurve", expData = expData,
-#'   partitionQSPR = "All Schmitt",
-#'   logLipo = 5,
-#'   ionization = c("acid", 0, 0),
-#'   typeSystem = "hepatocytes",
-#'   FBS = 0.0, pKa = c(3, 0, 0),
-#'   hlcAt = 1E-6,
-#'   microplateType = 96,
-#'   volMedium = 0.2,
-#'   cCells = 0.02
-#' )
 #'
-clearance_IVIVE <- function(typeValue, units, expData, typeSystem, partitionQSPR, logLipo,
-                            FBS, microplateType = NULL, ionization = NULL, volMedium = NULL,
-                            REF = NULL, hlcAt = NULL,
-                            pKa = NULL, fu = NULL, fu_hep = NULL, cMicro = NULL, cCells = NULL,
-                            BP = NULL, species = NULL) {
+#' example microsomes
+#' clearance_IVIVE(typeValue="in vitro clearance parameter",typeSystem="microsomes",units="mL/seconds",
+#'                 expData=18.27,fu_invitro=0.5,cMicro=0.5,volMedium=0.5)
+#'
+#'
+
+clearance_IVIVE<-function(typeValue,units,expData,typeSystem,fu_invitro,tissue=NULL,species=NULL,volMedium=NULL,
+                          REF=NULL,cMicro=NULL,cCells=NULL){
+
   # check if the arguments are valid
-  rlang::arg_match(typeSystem, c(
-    "hepatocytes",
-    "microsomes"
-  ))
+  rlang::arg_match(typeSystem, c("hepatocytes",
+                                  "microsomes"))
 
-  rlang::arg_match(typeValue, c(
-    "decay experimentalcurve",
-    "halfLife",
-    "in vitro clearance parameter"
-  ))
+  rlang::arg_match(typeValue, c("decay experimentalcurve",
+                                "halfLife",
+                                "in vitro clearance parameter" ))
 
-  # defaults for empty variable
-  if (is.null(pKa)) {
-    pKa <- c(0, 0, 0)
-  } else {}
-
-  if (is.null(hlcAt)) {
-    hlcAt <- 1E-6
-  } else {}
-
-  if (is.null(fu)) {
-    fu <- 0.2
-  } else {}
-
-  if (is.null(BP)) {
-    BP <- 1
-  } else {}
+  #defaults for empty variable
+  if (is.null(fu_invitro))  {
+    fu_invitro<-1
+  }else if (fu_invitro==0){
+    print("problem fu_invitro=0")
+    } else {}
 
   if (is.null(REF)) {
-    REF <- 1
+    REF<-1
+  }else{}
+
+  if (is.null(species))  {
+    species<-"human"
+  }else{}
+
+  #make it accordingly to microplate
+  if (is.null(volMedium))  {
+    volMedium<-1
+  }else{}
+
+  if (is.null(tissue))  {
+    tissue<-"liver"
+  }else{}
+
+   #get table values  for species
+  #Get the scaling factors
+  if (typeSystem=="microsomes"){
+
+    nLiver<-40  # mg/g liver
+    cInvitro<-cMicro    #mg/mL
+
+  } else if (typeSystem=="hepatocytes"){
+
+    nLiver<-139  # million cells/g liver
+    cInvitro<-cCells # million cells/mL assay
+
   } else {}
 
-  if (is.null(species)) {
-    species <- "human"
-  } else {}
+  #Derive the in vitro clearance value
+  if (typeValue=="decay experimentalcurve"){
 
-  # make it accordngly to microplate
-  if (is.null(volMedium)) {
-    volMedium <- 1
-  } else {}
+    kcat_min<-determineClearance(expData)[1]
+    clinvitro<-kcat_min/cInvitro
 
+  } else if (typeValue=="halfLife") {
 
-  if (is.null(fu_hep)) {
-    # get fu_hep
-    if (typeSystem == "microsomes") {
-      fu_hep <- as.double(FractionUnbound(
-        partitionQSPR = partitionQSPR,
-        logLipo = logLipo,
-        hlcAt = hlcAt, ionization = ionization,
-        typeSystem = "microsomes", FBS = FBS,
-        microplateType = microplateType,
-        volMedium = volMedium, pKa = pKa,
-        BP = BP, fu = fu, cMicro = cMicro
-      ))
-    } else if (typeSystem == "hepatocytes") {
-      fu_hep <- as.double(FractionUnbound(
-        partitionQSPR = partitionQSPR,
-        logLipo = logLipo,
-        hlcAt = hlcAt, ionization = ionization,
-        typeSystem = "hepatocytes", FBS = FBS,
-        microplateType = microplateType,
-        volMedium = volMedium, pKa = pKa,
-        BP = BP, fu = fu,
-        cCells = cCells
-      ))
-    } else {}
-  } else {}
+     halfLife<-expData
 
-  if (fu_hep == 0) {
-    print("problem fu_hep=0")
-  } else {}
+    #make matrix of calculations depedning on the units
+    matrixCalcHalf<-cbind(c("minutes","hours","seconds"),
+                          c(1,1/60,60))
 
-  # Get the scaling factors
-  if (typeSystem == "microsomes") {
-    nLiver <- 40 # mg/g liver
-    cInvitro <- cMicro # mg/mL
-  } else if (typeSystem == "hepatocytes") {
-    nLiver <- 139 # million cells/g liver
-    cInvitro <- cCells # million cells/mL assay
-  } else {}
+    multFactorHalf<-as.double(matrixCalcHalf[which(matrixCalcHalf[,1]==units),2])
+    kcat_min<-0.693/halfLife*multFactorHalf
+    clinvitro<-kcat_min/cInvitro
 
-  # Derive the in vitro clearance value
-  if (typeValue == "decay experimentalcurve") {
-    kcat <- determineClearance(expData)[1]
-    clinvitro <- kcat / cInvitro
-  } else if (typeValue == "halfLife") {
-    halfLife <- expData
+  } else if (typeValue=="in vitro clearance parameter") {
 
-    # make matrix of calculations depedning on the units
-    matrixCalcHalf <- cbind(
-      c("minutes", "hours", "seconds"),
-      c(1, 1 / 60, 60)
-    )
+    #make matrix of calculations depending on the units
+    matrixCalClear<-cbind(c("/minutes","/hours","/seconds",
+                            "mL/minutes/Millioncells", "uL/minutes/Millioncells", "L/minutes/Millioncells",
+                            "mL/hours/Millioncells", "uL/hours/Millioncells", "L/hours/Millioncells",
+                            "mL/seconds/Millioncells","uL/seconds/Millioncells","L/seconds/Millioncells",
+                            "mL/minutes/cell","uL/minutes/cell",
+                            "mL/hours/cell", "uL/hours/cell",
+                            "mL/seconds/cell","uL/seconds/cell",
+                            "mL/minutes/mg protein", "uL/minutes/mg protein","L/minutes/mg protein",
+                            "mL/hours/mg protein", "uL/hours/mg protein","L/hours/mg protein",
+                            "mL/seconds/mg protein","uL/seconds/mg protein","L/seconds/mg protein",
+                            "mL/minutes","uL/minutes",#not sure if correct
+                            "mL/seconds","uL/seconds",
+                            "mL/hours","uL/hours"),
+                          c(1/cInvitro,1/cInvitro*60,1/cInvitro/60,
+                            1,1/1000,1000,
+                            1/60,1/60000,1000/60,
+                            60,60/1000,60000,
+                            1E6,1000,
+                            1E6/60,1E6/60000,
+                            60*1E6,60*1E6/1000,
+                            1,1/1000,1000,
+                            1/60,1/60000,1000/60,
+                            60,60/1000,60000,
+                            1/(cInvitro*volMedium),1/(cInvitro*volMedium)/1000,
+                            1/(cInvitro*volMedium)*60,1/(cInvitro*volMedium)*60/1000,
+                            1/(cInvitro*volMedium)/60,1/(cInvitro*volMedium)/60000
+                            ))
 
-    multFactorHalf <- as.double(matrixCalcHalf[which(matrixCalcHalf[, 1] == units), 2])
-    kcat <- 0.693 / halfLife * multFactorHalf
-    clinvitro <- kcat / cInvitro
-  } else if (typeValue == "in vitro clearance parameter") {
-    # make matrix of calculations depending on the units
-    matrixCalClear <- cbind(
-      c(
-        "/minutes", "/hours", "/seconds",
-        "mL/minutes/Millioncells", "uL/minutes/Millioncells", "L/minutes/Millioncells",
-        "mL/hours/Millioncells", "uL/hours/Millioncells", "L/hours/Millioncells",
-        "mL/seconds/Millioncells", "uL/seconds/Millioncells", "L/seconds/Millioncells",
-        "mL/minutes/cell", "uL/minutes/cell",
-        "mL/hours/cell", "uL/hours/cell",
-        "mL/seconds/cell", "uL/seconds/cell",
-        "mL/minutes/mg protein", "uL/minutes/mg protein", "L/minutes/mg protein",
-        "mL/hours/mg protein", "uL/hours/mg protein", "L/hours/mg protein",
-        "mL/seconds/mg protein", "uL/seconds/mg protein", "L/seconds/mg protein",
-        "mL/minutes", "uL/minutes", # not sure if correct
-        "mL/seconds", "uL/seconds",
-        "mL/hours", "uL/hours"
-      ),
-      c(
-        1 / cInvitro, 1 / cInvitro * 60, 1 / cInvitro / 60,
-        1, 1 / 1000, 1000,
-        1 / 60, 1 / 60000, 1000 / 60,
-        60, 60 / 1000, 60000,
-        1E6, 1000,
-        1E6 / 60, 1E6 / 60000,
-        60 * 1E6, 60 * 1E6 / 1000,
-        1, 1 / 1000, 1000,
-        1 / 60, 1 / 60000, 1000 / 60,
-        60, 60 / 1000, 60000,
-        1 / (cInvitro * volMedium), 1 / (cInvitro * volMedium) / 1000,
-        1 / (cInvitro * volMedium) * 60, 1 / (cInvitro * volMedium) * 60 / 1000,
-        1 / (cInvitro * volMedium) / 60, 1 / (cInvitro * volMedium) / 60000
-      )
-    )
+    multFactorClear<-as.double(matrixCalClear[which(matrixCalClear[,1]==units),2])
 
-    multFactorClear <- as.double(matrixCalClear[which(matrixCalClear[, 1] == units), 2])
-    clinvitro <- expData * multFactorClear # mL/min/million cells
+    if (length(multFactorClear)==0) {
+      print("Not identified units") } else {}
+
+    clinvitro<-expData*multFactorClear  #mL/min/million cells
+
   } else {
-    clinvitro <- 0
+    clinvitro<-0
   }
 
-  # fraction of liver that is cells
-  fintcell_liver <- 0.67
-  # Calculate Clearance for unb fraction
-  clinvitroU <- clinvitro / fu_hep
-  ClspePermin <- as.double(clinvitroU * nLiver / fintcell_liver * REF)
-  print(ClspePermin)
+  #Fraction intracellular
+  fintcell_db<-c("brain"=0.96,"fat"=0.82,"gonads"=0.88,
+              "heart"=0.76,"kidney"=0.57,"gut"=0.88,"liver"=0.67,
+              "lung"=0.23)
+
+  fintcell<-as.double(fintcell_db[which(names(fintcell_db)==tissue)])
+
+  #Calculate Clearance for unb fraction
+  clinvitroU<- clinvitro/fu_invitro
+  ClspePermin<-as.double(clinvitroU*nLiver/fintcell*REF)
+
+  return("ClspePermin"=ClspePermin)
+
 }
 
-# function to determine clearance from experimental curve
-#' Title
-#'
-#' @param expData
-#'
-#' @return
-#' @export
-#' @import ggplot2
-#'
-#' @examples
-determineClearance <- function(expData) {
-  # Load the depletion curve
-  clear_curve_xy <- expData
-  colnames(clear_curve_xy) <- c("x", "y")
+#function to determine clearance from experimental curve
+determineClearance<-function(expData)  {
 
-  # find the starting concentration
-  y0 <- mean(clear_curve_xy$y[clear_curve_xy$x == 0])
+  library(ggplot2)
 
-  # create clearance model
-  Kcat_function <- function(y0, x, Kcat) {
-    y <- y0 * exp(-Kcat * x)
+  #Load the depletion curve
+  clear_curve_xy<-expData
+  colnames(clear_curve_xy)<-c("x","y")
+
+  #find the starting concentration
+  y0=mean(clear_curve_xy$y[clear_curve_xy$x==0])
+
+  #create clearance model
+  Kcat_function <- function(y0,x,Kcat) {
+    y=y0*exp(-Kcat*x)
     return(y)
   }
 
-  # fit model
-  fitKcat <- nls(y ~ Kcat_function(y0, x, Kcat),
-    data = clear_curve_xy,
-    start = list(Kcat = 0.01),
-    trace = TRUE
-  )
+  #fit model
+  fitKcat <- nls(y ~  Kcat_function(y0,x,Kcat),
+                 data = clear_curve_xy,
+                 start=list(Kcat=0.01),
+                 trace=TRUE)
 
 
-  colnames(clear_curve_xy) <- colnames(expData)
-  # Plot for evaluating if fit is reasonable
-  diag_plot <- ggplot(clear_curve_xy, aes(x = Time_min, y = Concentration_uM)) +
-    geom_point() +
-    labs(title = "fit curve") +
-    stat_function(fun = function(x) Kcat_function(x, y0 = y0, Kcat = coefficients(fitKcat)), colour = "blue")
+  colnames(clear_curve_xy)<-colnames(expData)
+  #Plot for evaluating if fit is reasonable
+  diag_plot<-ggplot(clear_curve_xy,aes(x=Time_min,y=Concentration_uM))+
+    geom_point()+
+    labs(title="fit curve")+
+    stat_function(fun = function(x) Kcat_function(x,y0=y0,Kcat=coefficients(fitKcat)), colour = "blue")
 
   print(diag_plot)
 
-  # Make table with fit Kcat
-  fit_95conf <- confint(fitKcat)
-  kcat <- as.double(data.frame("Kcat_permin" = coefficients(fitKcat)["Kcat"], fit_95conf[1], fit_95conf[2]))
-  names(kcat) <- c("Mean", "2.5%_CI", "95%_CI")
+  #Make table with fit Kcat
+  fit_95conf=confint(fitKcat)
+  kcat=as.double(data.frame("Kcat_permin"=coefficients(fitKcat)["Kcat"],fit_95conf[1],fit_95conf[2]))
+  names(kcat)=c("Mean","2.5%_CI","95%_CI")
 
   return(kcat)
+
 }
+
+
+
