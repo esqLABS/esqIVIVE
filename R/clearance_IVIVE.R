c213395fe766b27d1cf3fc62ad46e314a46ac841
Susana Proença <susana.proenca@esqlabs.com>
2025-07-17 20:54:47 +0200

    add wood scalars and some species specific parameters

--- R/clearance_IVIVE.R
+++ R/clearance_IVIVE.R
@@ -10,31 +10,32 @@
 #' @param volMedium volume of medium in the well (in mL)
 #' @param REF relative expression or activity factor
 #' @param fu_invitro Fraction unbound in the in vitro hepatic system. if not known code will calculate it
-#' @param cMicro concentration of microsome protein mg/mL
-#' @param cCells concentration of hepatocytes used million cells/mL
+#' @param cMicro_mgml concentration of microsome protein mg/mL
+#' @param cCells_Mml concentration of hepatocytes used million cells/mL
 #' @param species values can human and rat for now, defaulting to human
 #' @param tissue tissue of interest, since scaling factors are dependent on the tissue, will default to liver
+#' @param empirical_scalar , this is an option to include an extra empirical correction factor. Currently we are sondiering the scale factor of Wood 2017
 #'
 #'@return  Specific clearance parameter to plug in PK-Sim
 #'@examples
 #'
-#'expData<-read.csv("tests/Clearance_example.csv",header=TRUE)
-#'clearance_IVIVE(typeValue="decay experimentalcurve",typeSystem="hepatocytes",cCells=0.5,units="/minutes",
-#'                expData=expData,fu_invitro=0.5)
+#'expData<-read.csv("tests/testthat/data/clearance.csv",header=TRUE)
+#'clearance_IVIVE(typeValue="decay experimentalcurve",typeSystem="hepatocytes",cCells_Mml=0.5,units="/minutes",
+#'               expData=expData,fu_invitro=0.5,empirical_scalar="No")
 #'
 #'example hepatocytes
-#' clearance_IVIVE(typeValue="in vitro clearance parameter",typeSystem="hepatocytes",units="mL/minutes/Millioncells",
-#' expData=18.27,fu_invitro=0.5,cCells=0.5)
+#'clearance_IVIVE(typeValue="in vitro clearance parameter",typeSystem="hepatocytes",species="human",units="mL/minutes/Millioncells",
+#'expData=18.27,fu_invitro=0.5,cCells_Mml=0.5,empirical_scalar="No")
 #'
 #'
 #' example microsomes
-#' clearance_IVIVE(typeValue="in vitro clearance parameter",typeSystem="microsomes",units="mL/seconds",
-#'                 expData=18.27,fu_invitro=0.5,cMicro=0.5,volMedium=0.5)
+#'clearance_IVIVE(typeValue="in vitro clearance parameter",typeSystem="microsomes",units="L/minutes/mg protein",
+#'                expData=18.27,fu_invitro=0.5,cMicro=0.5,volMedium=0.5,empirical_scalar="No")
 #'
 #'
 
-clearance_IVIVE<-function(typeValue,units,expData,typeSystem,fu_invitro,tissue=NULL,species=NULL,volMedium=NULL,
-                          REF=NULL,cMicro=NULL,cCells=NULL){
+clearance_IVIVE<-function(typeValue,units,expData,typeSystem,fu_invitro,empirical_scalar,tissue=NULL,species=NULL,volMedium=NULL,
+                          REF=NULL,cMicro_mgml=NULL,cCells_Mml=NULL){
 
   # check if the arguments are valid
   rlang::arg_match(typeSystem, c("hepatocytes",
@@ -68,25 +69,63 @@ clearance_IVIVE<-function(typeValue,units,expData,typeSystem,fu_invitro,tissue=N
     tissue<-"liver"
   }else{}
 
-   #get table values  for species
-  #Get the scaling factors
+  
+  #Fraction intracellular per organ
+  
+  scaling_factors<-list()
+  scaling_factors[["human"]]<-data.frame("organs"=c("brain","gonads",
+                                         "heart","kidney","gut","liver","lung"),
+                                        "fcell"=c(0.96,0.88,0.76,0.57,0.88,0.67,0.23),
+                                          "weightorgankgBW"=c(22.5,0.167,5.5,6.67,18.5,32,15),
+                                           "MPGO"=rep(36,7),       #mg microsomal protein/g liver
+                                           "HGO"=rep(119,7))       #million cells/g liver
+                                              
+  
+  scaling_factors[["rat"]]<-data.frame("organs"=c("brain","gonads",
+                                                    "heart","kidney","gut","liver","lung"),
+                                         "fcell"=c(0.96,0.79,0.64,0.7,0.88,0.72,0.19),
+                                         "weightorgankgBW"=c(7.39,10,3.47,10,31,43,4.34),
+                                         "MPGO"=rep(50,7),       #mg microsomal protein/g liver
+                                         "HGO"=rep(122,7))       #million cells/g liver
+                                
+ 
+   scaling_factors[["dog"]]<-data.frame("organs"=c("brain","gonads",
+                                                  "heart","kidney","gut","liver","lung"),
+                                       "fcell"=c(0.96,0.79,0.64,0.7,0.88,0.72,0.19),#factor of the rat
+                                       "weightorgankgBW"=c(5.88,0.36,5.1,11.7,17.5,25,10.3),
+                                       "MPGO"=rep(50,7),       #mg microsomal protein/g liver
+                                       "HGO"=rep(201,7))       #million cells/g liver
+
+  
+
+  #Get species scaling factor
+  SF_matrix<-scaling_factors[[species]]
+  fintcell<-SF_matrix[which(SF_matrix[,1]==tissue),"fcell"]
+  liverkgBW<-SF_matrix[which(SF_matrix[,1]==tissue),"weightorgankgBW"]#is only used in some cases
+  
+  
+  #chose the system specific scaling factors
   if (typeSystem=="microsomes"){
-
-    nLiver<-40  # mg/g liver
-    cInvitro<-cMicro    #mg/mL
-
-  } else if (typeSystem=="hepatocytes"){
-
-    nLiver<-139  # million cells/g liver
-    cInvitro<-cCells # million cells/mL assay
+    
+        nLiver<-SF_matrix[which(SF_matrix[,1]==tissue),"MPGO"] # mg/g liver
+        cInvitro<-cMicro   #mg/mL
+    
+   } else if (typeSystem=="hepatocytes"){
+    
+        nLiver<-SF_matrix[which(SF_matrix[,1]==tissue),"HGO"]
+        cInvitro<-cCells_Mml # million cells/mL assay
 
   } else {}
 
-  #Derive the in vitro clearance value
+  #combination scale factors
+  SF<-nLiver/fintcell*REF/fu_invitro
+  SF2<-1/fintcell*REF/fu_invitro
+  
+  #Derive the in vitro clearance value---------------------
   if (typeValue=="decay experimentalcurve"){
 
     kcat_min<-determineClearance(expData)[1]
-    clinvitro<-kcat_min/cInvitro
+    ClspePermin<-kcat_min/cInvitro*SF
 
   } else if (typeValue=="halfLife") {
 
@@ -98,7 +137,8 @@ clearance_IVIVE<-function(typeValue,units,expData,typeSystem,fu_invitro,tissue=N
 
     multFactorHalf<-as.double(matrixCalcHalf[which(matrixCalcHalf[,1]==units),2])
     kcat_min<-0.693/halfLife*multFactorHalf
-    clinvitro<-kcat_min/cInvitro
+    ClspePermin<-kcat_min/cInvitro*SF
+
 
   } else if (typeValue=="in vitro clearance parameter") {
 
@@ -113,7 +153,7 @@ clearance_IVIVE<-function(typeValue,units,expData,typeSystem,fu_invitro,tissue=N
                             "mL/minutes/mg protein", "uL/minutes/mg protein","L/minutes/mg protein",
                             "mL/hours/mg protein", "uL/hours/mg protein","L/hours/mg protein",
                             "mL/seconds/mg protein","uL/seconds/mg protein","L/seconds/mg protein",
-                            "mL/minutes","uL/minutes",#not sure if correct
+                            "mL/minutes","uL/minutes",
                             "mL/seconds","uL/seconds",
                             "mL/hours","uL/hours",
                             "mL/minutes/kg","uL/minutes/kg","mL/hours/kg","uL/hours/kg"),
@@ -130,31 +170,53 @@ clearance_IVIVE<-function(typeValue,units,expData,typeSystem,fu_invitro,tissue=N
                             1/(cInvitro*volMedium),1/(cInvitro*volMedium)/1000,
                             1/(cInvitro*volMedium)*60,1/(cInvitro*volMedium)*60/1000,
                             1/(cInvitro*volMedium)/60,1/(cInvitro*volMedium)/60000,
-                            1/32/0.67,1/32/0.67/1000,1/32/0.67/60,1/32/0.67/60000)) #multiplying by the g liver weight per kilo BW
+                            1/liverkgBW,1/liverkgBW/1000,
+                            1/liverkgBW/60,1/liverkgBW/60000)) #multiplying by the g liver weight per kilo BW
+    
+    matrixCalClear[1:33,2]<-as.double(matrixCalClear[1:33,2])*SF
+    matrixCalClear[34:37,2]<-as.double(matrixCalClear[34:37,2])*SF2
+    
+    multFactorCl<-as.double(matrixCalClear[which(matrixCalClear[,1]==units),2])
 
-    multFactorClear<-as.double(matrixCalClear[which(matrixCalClear[,1]==units),2])
+      if (length(multFactorCl)==0) {
+        print("Not identified units") } else {}
 
-    if (length(multFactorClear)==0) {
-      print("Not identified units") } else {}
-
-    clinvitro<-expData*multFactorClear  #mL/min/million cells
+    ClspePermin<-expData*multFactorCl
+ 
 
   } else {
-    clinvitro<-0
+    ClspePermin<-0
   }
 
-  #Fraction intracellular
-  fintcell_db<-c("brain"=0.96,"fat"=0.82,"gonads"=0.88,
-              "heart"=0.76,"kidney"=0.57,"gut"=0.88,"liver"=0.67,
-              "lung"=0.23)
-
-  fintcell<-as.double(fintcell_db[which(names(fintcell_db)==tissue)])
-
-  #Calculate Clearance for unb fraction
-  clinvitroU<- clinvitro/fu_invitro
-  ClspePermin<-as.double(clinvitroU*nLiver/fintcell*REF)
-
-  return("ClspePermin"=ClspePermin)
+  #Add scalars from Wood et al 2017-https://doi.org/10.1124/dmd.117.077040.
+  wood_sf<-list()
+  wood_sf[["human"]]<-data.frame(Cl_ranges=c("<10","10-100","100-1000","1000-1000",">10000"),
+                                 microsomes=c(0.7,1.8,4.6,7.5,58),
+                                 hepatocytes=c(0.61,3.9,7.1,22,1200))
+  wood_sf[["rat"]]<-data.frame(Cl_ranges=c("<10","10-100","100-1000","1000-1000",">10000"),
+                                 microsomes=c(0.086,0.83,1.7,2.5,230),
+                                 hepatocytes=c(0.13,1.6,3.2,7.2,180))
+  wood_table<-wood_sf[[species]]
+  
+  
+  if (empirical_scalar=="Yes"){
+    # conditional to pick the right wood scalar
+    # this scalar are empitical and they are based on in vitro data tending to overestimate slow clearance 
+    # and underpredict fast clearance
+      if(ClspePermin*liverkgBW<10){
+        ClspePermin<-ClspePermin*wood_table[which(wood_table[,1]=="<10"),which(colnames(wood_table)==typeSystem)]
+      } else if (ClspePermin*liverkgBW<100&&ClspePermin*liverkgBW>10){
+        ClspePermin<-ClspePermin*wood_table[which(wood_table[,1]=="10-100"),which(colnames(wood_table)==typeSystem)]
+      } else if (ClspePermin*liverkgBW<1000&&ClspePermin*liverkgBW>100){
+        ClspePermin<-ClspePermin*wood_table[which(wood_table[,1]=="100-1000"),which(colnames(wood_table)==typeSystem)]
+      } else if (ClspePermin*liverkgBW<10000&&ClspePermin*liverkgBW>1000){
+        ClspePermin<-ClspePermin*wood_table[which(wood_table[,1]=="1000-10000"),which(colnames(wood_table)==typeSystem)]
+      } else if (ClspePermin*liverkgBW>10000){
+        ClspePermin<-ClspePermin*wood_table[which(wood_table[,1]==">10000"),which(colnames(wood_table)==typeSystem)]
+      }  
+  } else {}
+    
+  return("ClspePermin"=as.double(ClspePermin))
 
 }
 
